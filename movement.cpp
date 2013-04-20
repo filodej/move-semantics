f659798ca0b94d4c8a4e55c7bac57d0c1de147a7
filodej <philode@gmail.com>
2013-04-20 23:22:39 +0200

    Add comments for #1 - #4 cases

--- movement.cpp
+++ movement.cpp
@@ -7,10 +7,14 @@
 template <class SrcT>
 void conversion_test( std::basic_string<SrcT> const& s1, std::basic_string<SrcT> const& s2 )
 {
+	// case 1: converting an lvalue in a single expression is OK
 	std::cout << "This is ok: " << convert<char>( s1 ).c_str() << std::endl;
+	// case 2: converting an rvalue (temporary) in a single expression is OK
 	std::cout << "This is ok: " << convert<char>( s1 + s2 ).c_str() << std::endl;
+	// case 3: converting an lvalue with a named converter<> is OK
 	converter<char> const t1( s1 );
 	std::cout << "This is ok: " << t1.c_str() << std::endl;
+	// case 4: converting an rvalue (temporary) with a named converter<> is BAD
 	converter<char> const t12( s1 + s2 );
 	std::cout << "Oooops!: " << t12.c_str() << std::endl;
 }
